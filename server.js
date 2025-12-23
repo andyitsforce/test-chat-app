1a6415cd8710611f723d9b8e4eefe2fac8d8f39e
CuongHo <cuongho803@gmail.com>
2025-12-23 20:55:26 +0700

    add vercel.json

--- server.js
+++ server.js
@@ -1,38 +1,131 @@
-var app = require('http').createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('okay!');
-})
-var io = require('socket.io')(app, {
-    allowEIO3: true
-});
-var siofu = require('socketio-file-upload');
-var fs = require('fs');
-var roomUsers = {};
+const roomUsers = {};
+// id -> socketId map // seperate app and web 
+const webActiveUsers = new Map();
+const appActiveUsers = new Map();
+
+var siofu = require('socketio-file-upload'),
+    fs = require('fs'),
+    app = require('http').createServer(function (req, res) {
+        const myURL = new URL(req.url, `http://${req.headers.host}`);
+        if (req.method === 'POST' && myURL.pathname === '/foreLogout') {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString();
+            });
+
+            req.on('end', () => {
+                if (req.headers['x-api-key'] !== 'HOTAIFKE4PGN6C0') {
+                    res.writeHead(401);
+                    res.end("Invalid API Key");
+                    return;
+                }
+
+                // If JSON
+                const data = JSON.parse(body);
+                const { user_id, token, device_type } = data;
+                const forced = data?.forced || false;
+                const socketId = device_type === 'web' ? webActiveUsers.get(user_id) : appActiveUsers.get(user_id);
+                console.log({
+                    user_id: user_id,
+                    socketId: socketId
+                });
+                if (socketId) {
+                    io.to(socketId).emit("customer:received:force-logout", {
+                        message: "You have been logged out from another device",
+                        newToken: token,
+                        forced: forced
+                    });
+                    const instance = io.of("/").sockets.get(socketId);
+                    console.log({ instance: instance });
+                    if (instance) {
+                        instance.disconnect(true);
+                    }
+                }
+
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ status: 'ok' }));
+            });
+
+
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('okay!');
+        }
+    }), io = require('socket.io')(app, {
+        allowEIO3: true
+    });
 
 app.listen(8080, function () {
     console.log('listening');
 });
 
+
+
 io.on('connection', function (socket) {
     /**********************************************
      CUSTOMER
      ***********************************************/
+    // single device login
+    socket.on('customer:start:newSession', function (details) {
+        try {
+            const { customerId, login_token, device_type } = details, socketId = socket.id;
+            console.log('New customer session started:', { customerId, login_token, socketId, device_type });
+            device_type === 'web' ? webActiveUsers.set(customerId, socket.id) : appActiveUsers.set(customerId, socket.id);
+            console.log({
+                device_type: device_type,
+                webActiveUsers: webActiveUsers,
+                appActiveUsers: appActiveUsers
+            });
+        } catch (e) {
+            console.log("ERROR:" + e);
+        }
+
+    });
+
+    socket.on("customer:disconnectSession", () => {
+        for (const [u, s] of webActiveUsers.entries()) {
+            if (s === socket.id) webActiveUsers.delete(u);
+        }
+        for (const [u, s] of webActiveUsers.entries()) {
+            if (s === socket.id) appActiveUsers.delete(u);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        for (const [u, s] of webActiveUsers.entries()) {
+            if (s === socket.id) webActiveUsers.delete(u);
+        }
+         for (const [u, s] of appActiveUsers.entries()) {
+            if (s === socket.id) appActiveUsers.delete(u);
+        }
+    });
+
+    // end single device login
+
+
     socket.on('newCustomerConneted', function (details) {
         var index = details.customerData.uniqueId;
         roomUsers[index] = socket.id;
         Object.keys(roomUsers).forEach(function (key, value) {
             if (key === details.receiverUniqueId) {
                 let receiverSocketId = roomUsers[key];
-                socket.broadcast.to(receiverSocketId).emit('seller:received:refresh-seller-chat-list', details);
+                socket.broadcast.to(receiverSocketId).emit('seller:received:refresh-s eller-chat-list', details);
             }
         });
     });
+
+
     socket.on('customer:send:new-message', function (data) {
         try {
             if (typeof (data) !== 'undefined') {
                 Object.keys(roomUsers).forEach(function (key, value) {
+                    console.log({
+                        roomUsers: roomUsers,
+                        data: data
+                    })
                     if (key === data.receiverUniqueId) {
                         let receiverSocketId = roomUsers[key];
+
                         socket.broadcast.to(receiverSocketId).emit('seller:received:new-message', data);
                     }
                 });
@@ -173,9 +266,14 @@ io.on('connection', function (socket) {
     socket.on('seller:send:new-message', function (data) {
         try {
             if (typeof (data) !== 'undefined') {
+                console.log({
+                    roomUsers: roomUsers,
+                    data: data
+                })
                 Object.keys(roomUsers).forEach(function (key, value) {
+
                     if (key === data.receiverUniqueId) {
-                        receiverSocketId = roomUsers[key];
+                        const receiverSocketId = roomUsers[key];
                         socket.broadcast.to(receiverSocketId).emit('customer:received:new-message', data);
                     }
                 });
@@ -233,3 +331,4 @@ io.on('connection', function (socket) {
     });
 
 });
+
